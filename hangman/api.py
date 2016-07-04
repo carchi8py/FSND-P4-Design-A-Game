fa3b0d3a9b84f11b4697d2f9c8f42b47b944298e
Chris Archibald <carchi8py@gmail.com>
2016-07-04 11:14:11 -0700

    Added the ability to create a new game

--- hangman/api.py
+++ hangman/api.py
@@ -11,29 +11,51 @@ from protorpc import remote, messages
 from google.appengine.api import memcache
 from google.appengine.api import taskqueue
 
-from models import User
-from models import StringMessage
+from models import User, Game
+from models import StringMessage, NewGameForm, GameForm
 
-USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
-                                           email=messages.StringField(2))
+USER_REQUEST = endpoints.ResourceContainer(user_name = messages.StringField(1),
+                                           email = messages.StringField(2))
+NEW_GAME_REQUEST = endpoints.ResourceContainer(NewGameForm)
 
 @endpoints.api(name = "hangman", version = "v1")
 class Hangman(remote.Service):
-    @endpoints.method(request_message=USER_REQUEST,
-                      response_message=StringMessage,
-                      path='user',
-                      name='create_user',
-                      http_method='POST')
+    @endpoints.method(request_message = USER_REQUEST,
+                      response_message = StringMessage,
+                      path = 'user',
+                      name = 'create_user',
+                      http_method = 'POST')
     def create_user(self, request):
         """
         Creates a new User. Username is required
         """
         if User.query(User.name == request.user_name).get():
             raise endpoints.ConflictException(
-                "Username: %s is already taken!" % User.name)
+                "Username: %s is already taken!" % request.user_name)
         user = User(name = request.user_name, email = request.email)
         user.put()
         return StringMessage(message = "User {} created".format(
                 request.user_name))
+
+    @endpoints.method(request_message = NEW_GAME_REQUEST,
+                      response_message = GameForm,
+                      path = 'game',
+                      name = 'new_game',
+                      http_method = 'POST')
+    def new_game(self, request):
+        """
+        Creates a new hangman game. The word will be selected randomly from
+        a file
+        """
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                    "Username: %s not found" % request.user_name)
+        try:
+            game = Game.new_game(user.key, request.target, request.attempts)
+        except ValueError:
+            raise endpoints.BadRequestException("Target word cannot be null")
+        return game.to_form("Good Luck")
+
 
 api = endpoints.api_server([Hangman])
--- hangman/models.py
+++ hangman/models.py
@@ -7,6 +7,11 @@ from datetime import date
 from protorpc import messages
 from google.appengine.ext import ndb
 
+#list of random words to use if the user hasn't given us a word
+WORDS = ["horse", "door", "baseball", "dominoes",
+            "hockey", "aircraft", "password",
+            "gingerbread", "shallow", "lightsaber"]
+
 class User(ndb.Model):
     """
     User Profile
@@ -16,6 +21,74 @@ class User(ndb.Model):
     wins = ndb.IntegerProperty(default = 0)
     loses = ndb.IntegerProperty(default = 0)
 
+class Game(ndb.Model):
+    """
+    Game Object
+    """
+    target = ndb.StringProperty(required = True)
+    revealed_word = ndb.StringProperty(required = True)
+    attempts_allowed = ndb.IntegerProperty(required = True)
+    attempts_remaining = ndb.IntegerProperty(required = True, default = 5)
+    game_over = ndb.BooleanProperty(required = True, default = False)
+    user = ndb.KeyProperty(required = True, kind = 'User')
+    moves = ndb.StringProperty(repeated = True)
+
+    @classmethod
+    def new_game(cls, user, target, attempts):
+        """
+        Creates a new game
+        """
+        #if the user hasn't given us a word use a random word
+        if not target:
+            target = WORDS[random.randint(0,9)]
+
+        game = Game(user = user,
+                    target = target,
+                    revealed_word = '*' * len(target),
+                    attempts_allowed = attempts,
+                    attempts_remaining = attempts,
+                    game_over = False,
+                    moves = [])
+        game.put()
+        return game
+
+    def to_form(self, message):
+        """
+        Returns a GameForm representation of the Game
+        """
+        form = GameForm()
+        form.urlsafe_key = self.key.urlsafe()
+        form.user_name = self.user.get().name
+        form.attempts_remaining = self.attempts_remaining
+        form.game_over = self.game_over
+        form.message = message
+        form.revealed_word = self.revealed_word
+        form.moves = self.moves
+        return form
+
+class GameForm(messages.Message):
+    """
+    GameForm for outbound game state information
+    """
+    urlsafe_key = messages.StringField(1, required = True)
+    attempts_remaining = messages.IntegerField(2, required = True)
+    game_over = messages.BooleanField(3, required = True)
+    message = messages.StringField(4, required = True)
+    user_name = messages.StringField(5, required = True)
+    revealed_word = messages.StringField(6, required = True)
+    moves = messages.StringField(7, repeated = True)
+
+class NewGameForm(messages.Message):
+    """
+    Used to create a new game
+    """
+    user_name = messages.StringField(1, required = True)
+    target = messages.StringField(2)
+    attempts = messages.IntegerField(3, default = 5)
+
+
 class StringMessage(messages.Message):
-    """StringMessage-- outbound (single) string message"""
+    """
+    StringMessage-- outbound (single) string message
+    """
     message = messages.StringField(1, required=True)
