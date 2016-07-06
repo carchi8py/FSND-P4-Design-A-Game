8d1c5f369611260d2d06f02c9112848ac27fd4bc
Chris Archibald <carchi8py@gmail.com>
2016-07-06 12:03:14 -0700

    Added a cron to the project

--- hangman/main.py
+++ hangman/main.py
@@ -14,12 +14,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import logging
 import webapp2
+from google.appengine.api import mail, app_identity
+from api import Hangman
+from models import User
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+class SendReminderEmail(webapp2.RequestHandler):
+    def get(self):
+        """
+        Send a reminder e-mail to users with e-mails address who has 
+        imcomplete games
+        """
+        app_id = app_identity.get_application_id()
+        users = user.query(User.email != None)
+        for user in users:
+            subject = "You have an unfinished game of Hangman"
+            body = "Hello {}, you have an unfinished game of hangman".format(user.name)
+            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+                           user.email,
+                           subject,
+                           body)
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/crons/send_reminder', SendReminderEmail)
 ], debug=True)
